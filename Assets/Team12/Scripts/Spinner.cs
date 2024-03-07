410baf0680ae2d60e39582b570aea33ed6a0cb2c
Lucas Hourahine <luca5h1905@gmail.com>
2024-03-07 09:25:07 -0500

    added spinner script to namespace

--- Assets/Team12/Scripts/Spinner.cs
+++ Assets/Team12/Scripts/Spinner.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spinner : MonoBehaviour
+namespace team12
 {
-    Rigidbody2D rb;
-    // Start is called before the first frame update
-    void Start()
+    public class Spinner : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody2D>();
+        Rigidbody2D rb;
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            rb.MoveRotation(rb.rotation + 1);
+        }
     }
 
-    private void FixedUpdate()
-    {
-        rb.MoveRotation(rb.rotation + 1);
-    }
 }
